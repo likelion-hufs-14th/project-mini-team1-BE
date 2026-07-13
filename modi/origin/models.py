10fbe289e29253d9b68a0aa652617ea69a9b7af0
nutdaeun <goodnvery@naver.com>
2026-07-13 23:09:20 +0900

    Origin 모델 작성

---
+++ modi/origin/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Origin(models.Model):
+    appointment_code = models.IntegerField() #appointment 모델 업데이트 후 외래키로 변경
+    name = models.CharField(max_length=50)
+    origin_name = models.CharField(max_length=200)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    created_at = models.DateTimeField(auto_now_add=True)
