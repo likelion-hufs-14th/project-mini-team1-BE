1b06d3b0c7073918a859a3f0805eb64575564bb2
dangyulee <dg200101@gmail.com>
2026-07-15 18:31:10 +0900

    docs: swagger 추가

--- modi/modi/settings.py
+++ modi/modi/settings.py
@@ -39,8 +39,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'drf_spectacular',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Modi API',
+    'DESCRIPTION': 'Modi project API documentation',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
