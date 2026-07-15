27008224712b1f4964d3d7a70df31e6628380fdc
nutdaeun <goodnvery@naver.com>
2026-07-15 15:35:45 +0900

    필드명 수정

--- modi/origin/serializers.py
+++ modi/origin/serializers.py
@@ -4,5 +4,5 @@ from .models import Origin
 class OriginSerializer(serializers.ModelSerializer):
     class Meta:
         model = Origin
-        fields = ['appointment_code', 'name', 'origin_name', 'latitude', 'longitude', 'created_at']
+        fields = ['appointment_code', 'name', 'origin', 'latitude', 'longitude', 'created_at']
         read_only_fields = ['created_at']
--- modi/origin/views.py
+++ modi/origin/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import OriginSerializer
+from .models import Origin
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def origin_list(request, appointment_code):
+    if request.method == 'GET':
+        queryset = Origin.objects.filter(appointment_code=appointment_code)
+        serializer = OriginSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        data = request.data.copy()
+        data['appointment_code'] = appointment_code
+        serializer = OriginSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
