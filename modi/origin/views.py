931d41bd143928019154c49669985fc0a8308970
nutdaeun <goodnvery@naver.com>
2026-07-16 09:51:11 +0900

    수정사항 반영 - participants 모델 제거

--- modi/origin/views.py
+++ modi/origin/views.py
@@ -2,26 +2,19 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import OriginCreateSerializer, OriginResponseSerializer, CurrentOriginsSerializer
-from .models import Origin, Participant
+from .models import Origin
 # Create your views here.
 
 @api_view(['GET', 'POST'])
 def origin_list(request, appointment_code):
 
-
     if request.method == 'GET':
-        participants = Participant.objects.filter(
-            appointment_code=appointment_code
-        ).select_related('origin')
-
-        serializer = CurrentOriginsSerializer(participants, many=True)
+        origins = Origin.objects.filter(appointment_code=appointment_code)
+        serializer = CurrentOriginsSerializer(origins, many=True)
         return Response(serializer.data)
     
     elif request.method == 'POST':
-        serializer = OriginCreateSerializer(
-            data=request.data,
-            context={'appointment_code':appointment_code}
-        )
+        serializer = OriginCreateSerializer(request.data)
         if serializer.is_valid():
             origin = serializer.save()
             response_serializer = OriginResponseSerializer(origin)
