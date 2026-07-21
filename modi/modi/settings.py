5234f5df090784835597d3bd45a185fb503df373
nutdaeun <goodnvery@naver.com>
2026-07-21 21:56:33 +0900

    env 수정

--- modi/modi/settings.py
+++ modi/modi/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
+ODSAY_API_KEY = config('ODSAY_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
@@ -151,7 +152,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ODSAY_API_KEY="/A4oSsOdNvZlNpw4iYxt3WyqMYHQ0xIy0F6qhsOvGqg"
 
 try:
     from .local_settings import *
