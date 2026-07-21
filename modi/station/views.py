e26e223f1ca94fb972bba294467518d986251b18
nutdaeun <goodnvery@naver.com>
2026-07-21 15:06:17 +0900

    위경도를 DB에서 꺼내오도록 수정

--- modi/station/views.py
+++ modi/station/views.py
@@ -2,13 +2,17 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .services import StationRecommendationService
+from ..origin.models import Origin
 
 class StationCandidateView(APIView):
     def post(self, request):
-        # 프론트에서 전달받을 출발지 좌표 리스트
-        # 예: {"participants": [{"lat": 37.12, "lng": 127.54}, ...]}
-        participants_coords = request.data.get('participants', [])
-        
+        appointment_code = request.data.get("appointment_code")
+        origins = Origin.objects.filter(appointment_code=appointment_code)
+        participants_coords = [
+            {"lat": float(origin.latitude), "lng": float(origin.longitude)}
+            for origin in origins
+        ]
+
         if not participants_coords:
             return Response({"error": "참여자 좌표 데이터가 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
             
