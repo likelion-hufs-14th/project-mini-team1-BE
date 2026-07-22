638fae0d345b4bb31fcc7865da778fa92e6143e7
nutdaeun <goodnvery@naver.com>
2026-07-22 15:36:34 +0900

    fix

--- modi/modi/settings.py
+++ modi/modi/settings.py
@@ -86,6 +86,7 @@ CORS_ALLOWED_ORIGINS = [
 	'http://localhost:5173',
     'https://project-mini-team1-fe.vercel.app',
     'http://127.0.0.1:5173',
+    'https://likelion-14th-modi.vercel.app',
 ]
 
 ROOT_URLCONF = 'modi.urls'
